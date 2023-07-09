cf1aed74fc4ce2cef8ed80311b76761cab1b289c
dxlnr <daniel.illner@outlook.de>
2023-07-09 12:38:52 +0200

    pylsp custom configs

---
+++ editor/.config/nvim/lua/core/lsp/servers/pylsp.lua
@@ -0,0 +1,16 @@
+return {
+  settings = {
+    pylsp = {
+      plugins = {
+        pylint = {
+          args = { {'--ignore=E501,E231', '-'}, enabled=true, debounce=200 }
+        },
+        pycodestyle = {
+          enabled=true,
+          ignore={'E501', 'E231', 'E121', 'E126', 'W503'},
+          maxLineLength=96
+        },
+      }
+    }
+  }
+}
